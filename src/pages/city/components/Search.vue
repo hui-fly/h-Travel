1874df5c70f2e71f8912fadb2d88e5d65e64457f
hui-fly <z1561464710@gmail.com>
2018-08-14 22:13:11 +0800

    city search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="search">
+		<input class="search-input" type="text" placeholder="输入城市名或拼音">
+	</div>
+</template>
+<script>
+export default{
+	name:'CitySearch',
+}
+</script>
+<style scoped>
+	.search{
+		height:0.72rem;
+		padding: 0.1rem;
+		background: #00bcd4;
+	}
+	.search-input{
+		width: 100%;
+		height: 0.62rem;
+		line-height: 0.62rem;
+		text-align: center;
+		border-radius: 0.06rem;
+		color: #666;
+	}
+</style>
