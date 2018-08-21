ae88c595f3bd9437ebe39ca6b539e2ba6e4fb4e4
hui-fly <z1561464710@gmail.com>
2018-08-21 18:17:22 +0800

    city ajax

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,19 +1,14 @@
 <template>
-	<ul class="list">
-		<li class="item">A</li>
-		<li class="item">A</li>
-		<li class="item">A</li>
-		<li class="item">A</li>
-		<li class="item">A</li>
-		<li class="item">A</li>
-		<li class="item">A</li>
-		<li class="item">A</li>
-		<li class="item">A</li>
+	<ul class="list" >
+		<li class="item" v-for='(item,key) of cities'>{{key}}</li>
 	</ul>
 </template>
 <script>
 export default{
 	name:'CityAlphabet',
+	props:{
+		cities:Object,
+	}
 }
 </script>
 <style scoped>
